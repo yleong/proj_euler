e0ad003849543d1a7e23e0302736d958e42f8bd0
Yew Long Leong <yewlong42@gmail.com>
2015-10-27 00:08:46 +0800

    remove binary and add bash for 2

--- bash/2.sh
+++ bash/2.sh
@@ -2,3 +2,18 @@
 
 #I have an idea, I'll just generate it as I go along
 
+curr=1
+n=1
+nprev=1
+sum=0
+
+while [ $curr -lt 4000000 ] #this is quite like powershell
+do
+   curr=`expr $n + $nprev`
+   nprev=$n
+   n=$curr
+   if [ $(($curr % 2)) = 0 ]; then
+      sum=`expr $sum + $curr`
+   fi
+done
+echo $sum
